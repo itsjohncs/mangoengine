d00f7301515f5b78a447dd6e1ca56aa573c30ad3
brownhead <jsull003@ucr.edu>
2013-11-11 21:39:55 -0800

    Added additional documentation to fields.

--- mangoengine/errors.py
+++ mangoengine/errors.py
@@ -1,3 +1,12 @@
 class ValidationFailure(RuntimeError):
+    """
+    Occurs when the value given for a particular field is not valid.
+
+    For example, if the integer value 13 is given as the value for a
+    StringField, a ValidationFailure will be raised when
+    StringField.validate() is called.
+
+    """
+
     def __init__(self, *args, **kwargs):
         RuntimeError.__init__(self, *args, **kwargs)
--- mangoengine/fields.py
+++ mangoengine/fields.py
@@ -2,15 +2,29 @@
 import errors
 
 class Field(object):
+    """
+    A basic field. Can be inherited from.
+
+    :ivar nullable: If False, the value of this field cannot be None.
+
+    """
+
     _expected_type = None
+    """
+    The expected type of the field. Used internally by MangoEngine and can be
+    leveraged when creating your own fields.
+
+    """
 
     def __init__(self, nullable = False):
         self.nullable = nullable
 
     def validate(self, value):
+        # Ensure that if the value is None, it's OK
         if not self.nullable and value is None:
             raise errors.ValidationFailure("value cannot be None.")
 
+        # Ensure the that value is the corect type
         if (value is not None and
                 self._expected_type is not None and
                 not isinstance(value, self._expected_type)):
@@ -22,6 +36,11 @@ class Field(object):
             )
 
 class StringField(Field):
+    """
+    A string field.
+
+    """
+
     _expected_type = basestring
 
     def __init__(self, **default_kwargs):
@@ -31,6 +50,15 @@ class StringField(Field):
         super(StringField, self).validate(value)
 
 class DictField(Field):
+    """
+    A dictionary or map.
+
+    :ivar of_key: A the type of field that every key must be. Example, if this
+            is a StringField, all keys in the dictionary must be strings.
+    :ivar of_value: Similar to of_key but affecting the values.
+
+    """
+
     _expected_type = dict
 
     def __init__(self, of_key = None, of_value = None, **default_kwargs):
@@ -49,6 +77,14 @@ class DictField(Field):
         super(DictField, self).validate(value)
 
 class ListField(Field):
+    """
+    A list.
+
+    :ivar of: The type of field that each list item must be. For example, if
+            StringField is given, all list items must be strings.
+
+    """
+
     _expected_type = list
 
     def __init__(self, of = None, **default_kwargs):
