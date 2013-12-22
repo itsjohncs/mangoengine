a0de57ddef4da69bc68f5ae2e4accc0910a6c4d2
brownhead <jsull003@ucr.edu>
2013-12-22 14:33:59 -0800

    Trying out default RTD theme.
    * Looks like they've improved it considerably since the last time I
      checked (which was admittidely a very long time ago).

--- docs/conf.py
+++ docs/conf.py
@@ -26,7 +26,3 @@ exclude_patterns = ["_build"]
 
 # The name of the Pygments (syntax highlighting) style to use.
 pygments_style = "sphinx"
-
-# The theme to use for HTML and HTML Help pages.  See the documentation for
-# a list of builtin themes.
-html_theme = "nature"
